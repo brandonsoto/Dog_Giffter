8e595839838a664471a6eca8813c6ea58d46f3a0
Brandon Soto <brandon.soto09@gmail.com>
2017-08-23 18:33:02 -0700

    Add files

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog='Dog Giffer', description="A Reddit bot that shares cute dog gifs with Redditors that need it most.")
+parser.add_argument('-g', '--giphy', metavar="KEY", type=str, required=True, help="the GIPHY API key")
+args = parser.parse_args()
+
+def main():
+    data=json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + args.giphy + "&limit=25").read())
+    print json.dumps(data, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    print 'Starting app...'
+    main()
+    print 'Done!'
