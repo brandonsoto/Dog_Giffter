315f98dc949a52fa56ade36276cafcc8f3d562da
Brandon Soto <brandon.soto09@gmail.com>
2017-08-24 20:40:58 -0700

    Change file to run with python3

--- dog_giffter.py
+++ dog_giffter.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-import urllib
+import urllib.request
 import json
 import yaml
 
-credentials = yaml.load(file("credentials.yml", 'r'))
+credentials = yaml.load(open("credentials.yml", 'r'))
 
 def main():
-    data=json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + credentials["giphy"]["key"] + "&limit=25").read())
-    print json.dumps(data, sort_keys=True, indent=4)
+    data=json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + credentials["giphy"]["key"] + "&limit=25").read())
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 if __name__ == '__main__':
-    print 'Starting app...'
+    print('Starting app...')
     main()
-    print 'Done!'
+    print('Done!')
