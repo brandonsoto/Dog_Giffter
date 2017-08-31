f88d0b82910f336e874651a183e3bac56139944c
Brandon Soto <brandon.soto09@gmail.com>
2017-08-30 20:38:14 -0700

    Replace Docker/Vagrant with pipenv & replace urllib with requests

--- dog_giffter.py
+++ dog_giffter.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
-import urllib.request
+import requests
 import json
 import yaml
 
 credentials = yaml.load(open("credentials.yml", 'r'))
+giphy = credentials["giphy"]["key"]
+reddit = credentials["reddit"]
 
 def main():
-    data=json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + credentials["giphy"]["key"] + "&limit=25").read())
+    data = requests.get("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + giphy +
+            "&limit=25").json()
     print(json.dumps(data, sort_keys=True, indent=4))
 
 if __name__ == '__main__':
     print('Starting app...')
+    print('giphy key: ' + giphy)
+    print('reddit: ' + str(reddit))
     main()
     print('Done!')
