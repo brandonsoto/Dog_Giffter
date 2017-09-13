a1ce5a60d5452276c5ee0e753d272a174f9df8b2
Brandon Soto <brandon.soto09@gmail.com>
2017-09-12 21:49:21 -0700

    Integrate sentiment analysis
    Fixes #2

--- dog_giffter.py
+++ dog_giffter.py
@@ -1,45 +1,81 @@
 #!/usr/bin/env python
 
 import json
+from random import shuffle
 import praw
 import requests
 import yaml
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-credentials = yaml.load(open("credentials.yml", 'r'))
-gc = credentials["giphy"]["key"]  # giphy credentials
-rc = credentials["reddit"]  # reddit credentials
+nltk.download('vader_lexicon') # we don't use twitter, so we ignore the twypython warning
+
+ANALYZER = SentimentIntensityAnalyzer()
+CREDENTIALS = yaml.load(open("credentials.yml", 'r'))
+GC = CREDENTIALS["giphy"]["key"]  # giphy CREDENTIALS
+RC = CREDENTIALS["reddit"]  # reddit CREDENTIALS
+
+NEGATIVE_THRESHOLD = 0.5
+COMPOUND_THRESHOLD = -0.5
+
+dog_pictures = list()
+
+def get_pictures():
+    """
+    Retrieves dog gifs from GIPHY.
+    """
+    global dog_pictures
+    dog_pictures = requests.get("https://api.giphy.com/v1/gifs/search?api_key=" + GC + "&q=cute dog&limit=1000&offset=0&rating=G&lang=en").json()
+
+    if 'data' in dog_pictures:
+        dog_pictures = dog_pictures['data']
+        shuffle(dog_pictures)
+    else:
+        dog_pictures = list()
 
 
-# Performs analysis on a given comment
 def analyze_comment(comment):
-    # TODO: perform sentiment analysis on comment body here
-    print('################################################ START')
-    print(comment.body)
-    print('################################################ END\n')
-    # if in sentiment range
-    #     reply_to(comment)
-    pass
+    """
+    Performs sentiment analyzes on the given comment
+    """
+    scores = ANALYZER.polarity_scores(comment.body) 
+
+    if ( scores['compound'] < COMPOUND_THRESHOLD and scores['neg'] > NEGATIVE_THRESHOLD):
+        print(comment.body)
+        print(scores)
+        reply_to(comment)
+        print('==========================================================')
 
 
-# replies to the given comment with a cute dog gif
 def reply_to(comment):
-    # TODO: need to select picture and format reply text
-    dog_pictures = requests.get("http://api.giphy.com/v1/gifs/search?q=cute+dog&api_key=" + gc + "&limit=25").json()
-    print(json.dumps(dog_pictures, sort_keys=True, indent=4))
-    comment.reply("reply text goes here")
+    """
+    Writes a reply to the given comment. This reply contains a cute dog gif.
+    """
+
+    if not dog_pictures:
+        get_pictures()
+
+    if dog_pictures:
+        picture = dog_pictures.pop()
+        if 'images' in picture \
+        and 'fixed_width' in picture['images'] \
+        and 'url' in picture['images']['fixed_width']:
+            print('picture =', picture['images']['fixed_width']['url'])
+            # print(json.dumps(dog_pictures, sort_keys=True, indent=4))
+            # comment.reply("reply text goes here") TODO: uncomment this when reply is ready
 
 
 def main():
     # reddit instance
-    r = praw.Reddit(client_id=rc['client_id'], client_secret=rc['client_secret'], user_agent=rc['user_agent'],
-                    username=rc['username'], password=rc['password'])
+    reddit = praw.Reddit(client_id=RC['client_id'], client_secret=RC['client_secret'], user_agent=RC['user_agent'],
+                         username=RC['username'], password=RC['password'])
 
-    popular = r.subreddit('popular').controversial(limit=1)  # TODO: change limit when sentiment analysis is complete
+    popular = reddit.subreddit('popular').controversial(limit=1)  # TODO: change limit when sentiment analysis is complete
 
     for submission in popular:
         submission.comments.replace_more(limit=0)  # remove MoreComments instances
         print("---------------------------------------------------------------------")
-        print("title=", submission.title, ", id=", submission.id, ", url=", submission.url)
+        print("title=", submission.title, ", id=", submission.id, ", url=", submission.url, '\n')
         for comment in submission.comments.list():
             analyze_comment(comment)
         print("---------------------------------------------------------------------")
@@ -47,7 +83,7 @@ def main():
 
 if __name__ == '__main__':
     print('Starting app...')
-    print('giphy key: ' + gc)
-    print('reddit: ' + str(rc))
+    print('giphy key: ' + GC)
+    print('reddit: ' + str(RC))
     main()
     print('Done!')
